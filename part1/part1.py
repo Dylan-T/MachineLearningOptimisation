e43dbb1cc866dc34cd80fc6bf811057be0573bec
Dylan Thomson <thomsodyla1@myvuw.ac.nz>
2019-09-25 15:38:12 +1200

    standardized inputs

--- part1/part1.py
+++ part1/part1.py
@@ -19,33 +19,49 @@ warnings.simplefilter(action='ignore', category=FutureWarning)
 data = pd.read_csv("diamonds.csv")
 
 # Pre processing
-
-le = pp.LabelEncoder()
 # remove column 1 (Id's)
 data = data.drop([data.columns[0]],  axis='columns')
+
+le = pp.LabelEncoder()
+
 # cut
-le.fit(data['cut'])
+le.fit(["Fair", "Good", "Very Good", "Premium", "Ideal"])
 data['cut'] = le.transform(data['cut'])
+
 # color
 le.fit(data['color'])
 data['color'] = le.transform(data['color'])
+
 # clarity
-le.fit(data['clarity'])
+le.fit(["IF", "VVS1", "VVS2", "VS1", "VS2", "SI1", "SI2", "I1", "I2", "I3"])
 data['clarity'] = le.transform(data['clarity'])
-
-
 
 # Split 70:30
 train, test = ms.train_test_split(data, test_size=0.3, random_state=309)
 
+train_x = train.drop(['price'], axis=1)
+train_y = train['price']
+test_x = test.drop(['price'], axis=1)
+test_y = test['price']
+
+# Standardize the inputs
+train_mean = train_x.mean()
+train_std = train_x.std()
+train_x = (train_x - train_mean) / train_std
+test_x = (test_x - train_mean) / train_std
+
+# Tricks: add dummy intercept to both train and test
+train_x['intercept_dummy'] = pd.Series(1.0, index=train_x.index)
+test_x['intercept_dummy'] = pd.Series(1.0, index=test_x.index)
+
 # Create & Train Models
 LinearRegression = lm.LinearRegression()
-KNeighborsRegression = kn.KNeighborsRegressor(n_neighbors=5)
+KNeighborsRegression = kn.KNeighborsRegressor()
 RidgeRegression = lm.Ridge()
 DecisionTree = dt.DecisionTreeRegressor()
 RandomForest = en.RandomForestRegressor()
 GradientBoosting = en.GradientBoostingRegressor()
-SGD = lm.SGDRegressor(early_stopping=True)
+SGD = lm.SGDRegressor()
 SVR = svm.SVR()
 LinearSVR = svm.LinearSVR()
 MLP = nn.MLPRegressor()
@@ -53,29 +69,25 @@ MLP = nn.MLPRegressor()
 models = [LinearRegression, KNeighborsRegression, RidgeRegression, DecisionTree, RandomForest, GradientBoosting
           , SGD, SVR, LinearSVR, MLP]
 
-# train models
-for model in models:
-    start = time.time()
-    model.fit(train.loc[:, :'z'], train['price'])
-    print("--- %s training time: %s seconds ---" % (type(model).__name__, time.time() - start))
 
-# Test Models
 executionTimes = []
 predictions = []
 for model in models:
+    # train models
     start = time.time()
-    predictions.append(model.predict(test.loc[:, :'z']))
+    model.fit(train_x, train_y)
+    # Test Models
+    predictions.append(model.predict(test_x))
     executionTimes.append(round(time.time() - start, 2))
-trueValue = test['price']
 
 # Report evaluation metrics
 i = 0
 metrics = PrettyTable(["Model", "Execution Time", "MSE", "RMSE", "R-Squared", "MAE"])
 for prediction in predictions:
-    MSE = round(m.mean_squared_error(prediction, trueValue), 2)
+    MSE = round(m.mean_squared_error(prediction, test_y), 2)
     RMSE = round(np.sqrt(MSE), 2)
-    RSquared = round(m.r2_score(prediction, trueValue), 2)
-    MAE = round(m.mean_absolute_error(prediction, trueValue), 2)
+    RSquared = round(m.r2_score(prediction, test_y), 2)
+    MAE = round(m.mean_absolute_error(prediction, test_y), 2)
 
     metrics.add_row([type(models[i]).__name__, executionTimes[i], MSE, RMSE, RSquared, MAE])
     i += 1
--- part2/part2.py
+++ part2/part2.py
@@ -16,21 +16,26 @@ from prettytable import PrettyTable
 
 warnings.simplefilter(action='ignore', category=FutureWarning)
 
+# x = [["male", 0], ["female", 1]]
+# ohe = pp.OneHotEncoder(sparse=False)
+# ohe.fit(x)
+# print(ohe.transform(x))
+
 # Load Data
 train = pd.read_csv("adult.data", header=None)
 test = pd.read_csv("adult.test", header=None)
 
 # Pre processing
 # 15 features
-# 1 (Workclass)
-# 3 (Education)
-# 5 (Marital-status)
-# 6 (Occupation)
-# 7 (Relationship)
-# 8 (Race)
-# 9 (Sex)
-# 13 (native-country)
-# 14 (salary class)
+# 1 (Workclass) categorical
+# 3 (Education) ordinal
+# 5 (Marital-status) categorical
+# 6 (Occupation) categorical
+# 7 (Relationship) categorical
+# 8 (Race) categorical
+# 9 (Sex) binary
+# 13 (native-country) categorical
+# 14 (salary class) binary
 le = pp.LabelEncoder()
 
 for i in [1, 3, 5, 6, 7, 8, 9, 13, 14]:
@@ -39,51 +44,62 @@ for i in [1, 3, 5, 6, 7, 8, 9, 13, 14]:
     test[i] = le.transform(test[i])
 
 
+train_x = train.drop([14], axis=1)
+train_y = train[14]
+
+test_x = test.drop([14], axis=1)
+test_y = test[14]
+
+# Standardize the inputs
+train_mean = train_x.mean()
+train_std = train_x.std()
+train_x = (train_x - train_mean) / train_std
+test_x = (test_x - train_mean) / train_std
+
+# Tricks: add dummy intercept to both train and test ???
+train_x['intercept_dummy'] = pd.Series(1.0, index=train_x.index)
+test_x['intercept_dummy'] = pd.Series(1.0, index=test_x.index)
+
 # Create & Train Models
 kNN = kn.KNeighborsClassifier()
 NaiveBayes = nb.MultinomialNB()
-SVM = svm.LinearSVC() # t time scales at least quadratically with the number of samples and may be impractical beyond tens of thousands of samples
+SVM = svm.SVC(max_iter=1)
 DecisionTree = dt.DecisionTreeClassifier()
 RandomForest = en.RandomForestClassifier()
 AdaBoost = en.AdaBoostClassifier()
 GradientBoosting = en.GradientBoostingClassifier()
 LinearDA = da.LinearDiscriminantAnalysis()
-MLP = nn.MLPClassifier()
+MLP = nn.MLPClassifier(max_iter=1)
 LogisticRegression = lm.LogisticRegression()
 
 models = [kNN, NaiveBayes, SVM, DecisionTree, RandomForest, AdaBoost, GradientBoosting, LinearDA, MLP, LogisticRegression]
 
-# train models
-for model in models:
-    start = time.time()
-    model.fit(train.loc[:, :13], train[14])
-    print("--- %s training time: %s seconds ---" % (type(model).__name__, time.time() - start))
-
-
-# Test Models
 executionTimes = []
 predictions = []
+
 for model in models:
+    # train model
     start = time.time()
-    predictions.append(model.predict(test.loc[:, :13]))
+    model.fit(train_x, train_y)
+
+    # Test Model
+    predictions.append(model.predict(test_x))
     executionTimes.append(round(time.time() - start, 2))
-trueValue = test[14]
 
 # Report evaluation metrics
 i = 0
 metrics = PrettyTable(["Model", "Execution Time", "Accuracy", "Precision", "Recall", "F1-score", "AUC"])
 for prediction in predictions:
-    accuracy = round(m.accuracy_score(prediction, trueValue), 2)
-    precision = round(m.precision_score(prediction, trueValue), 2)
-    recall = round(m.recall_score(prediction, trueValue), 2)
-    f1Score = round(m.f1_score(prediction, trueValue), 2)
+    accuracy = round(m.accuracy_score(prediction, test_y), 2)
+    precision = round(m.precision_score(prediction, test_y), 2)
+    recall = round(m.recall_score(prediction, test_y), 2)
+    f1Score = round(m.f1_score(prediction, test_y), 2)
 
-    fpr, tpr, thresholds = m.roc_curve(trueValue, prediction)
+    fpr, tpr, thresholds = m.roc_curve(test_y, prediction)
     auc = round(m.auc(fpr, tpr), 2)
 
     metrics.add_row([type(models[i]).__name__, executionTimes[i], accuracy, precision, recall, f1Score, auc])
     i += 1
 
-metrics.sortby = "Accuracy"
 print(metrics)
 
